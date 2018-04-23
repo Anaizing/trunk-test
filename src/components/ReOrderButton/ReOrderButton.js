584d0c38cb2cd2bb2a2a0e65a0e1571d2d8ba2cc
Anai Araya <anai.yusary@gmail.com>
2018-04-23 22:20:55 +1000

    add reOrderButton

---
+++ src/components/ReOrderButton/ReOrderButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './ReOrderButton.css';
+
+class ReOrderButton extends Component {
+
+
+  render() {
+    return (
+      <div className="ReOrder">
+        <button>Change Order</button>
+      </div>
+    );
+  }
+}
+
+export default ReOrderButton;
