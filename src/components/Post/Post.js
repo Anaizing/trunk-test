e8367b4bf0d3e37c4692f370cf9f0901850435cf
Anai Araya <anai.yusary@gmail.com>
2018-04-23 22:13:29 +1000

    add post component

---
+++ src/components/Post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Post.css';
+
+const post = props => (
+  <article className="Post">
+    <a href={props.url} >
+      <h1>{props.title}</h1>
+      <div className="Info">
+        <div className="Author">Author: {props.author}</div>
+      </div>
+    </a>
+  </article>
+);
+
+export default post;
