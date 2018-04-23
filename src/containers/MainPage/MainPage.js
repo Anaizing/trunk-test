81b2956dd55f583e11ff9a9baa07f006f6d07880
Anai Araya <anai.yusary@gmail.com>
2018-04-23 22:56:34 +1000

    add logic to fetch reddit data

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+import Post from '../../components/Post/Post';
+import ReOrderButton from '../../components/ReOrderButton/ReOrderButton';
 import './MainPage.css';
 
 class MainPage extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount () {
+        axios.get('https://www.reddit.com/r/javascript/top/.json')
+            .then(response => {
+                this.setState({posts: response.data.data.children})
+                console.log(response);
+            })
+    }
+
     render () {
-        return 
-        <div>
-            <section className="Post">
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.data.id} title={post.data.title} author={post.data.author} url={post.data.url}/>
+            
+        })
+        return <li>
+            <section className="Posts">{posts}
             </section>
-        </div>
+            <section>
+                <ReOrderButton />
+            </section>
+        </li>;
     }
 }
 
