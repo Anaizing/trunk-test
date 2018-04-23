b2c28696a31f26abfc942f331c5dc82a466c4f84
Anai Araya <anai.yusary@gmail.com>
2018-04-23 22:10:58 +1000

    add containers folder with MainPage files

---
+++ src/containers/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './MainPage.css';
+
+class MainPage extends Component {
+    render () {
+        return 
+        <div>
+            <section className="Post">
+            </section>
+        </div>
+    }
+}
+
+export default MainPage;
