8d482102b1d696552b96686a197296fd24497d72
Anai Araya <anai.yusary@gmail.com>
2018-04-23 23:40:22 +1000

    add accent button color

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,7 +5,7 @@ import './Post.css';
 const post = props => (
   <article className="Post">
     <a href={props.url} >
-      <h1>{props.title}</h1>
+      <h1 className="Titles">{props.title}</h1>
       <div className="Info">
         <div className="Author">Author: {props.author}</div>
       </div>
